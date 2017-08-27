aba8d509d3b1253dd898b96d98dbf6c6b122768f
Varun Ramesh <varunramesh@live.com>
2017-08-26 23:12:03 -0700

    Tag videos.

---
+++ tag-videos.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import click
+import pinboard
+from urllib.parse import urlparse
+
+VIDEO_HOSTNAMES = set([
+    'www.vimeo.com',
+    'vimeo.com',
+    'www.youtube.com',
+    'youtube.com',
+    'youtu.be',
+])
+
+@click.command()
+@click.option('--auth-token', required=True)
+def tag_videos(auth_token):
+    pb = pinboard.Pinboard(auth_token)
+
+    with click.progressbar(pb.posts.all(), label="Finding videos...") as bar:
+        for bookmark in bar:
+            hostname = urlparse(bookmark.url).hostname
+            if hostname in VIDEO_HOSTNAMES:
+                if 'video' not in bookmark.tags:
+                    bookmark.tags.append('video')
+                    bookmark.save()
+
+
+if __name__ == '__main__':
+    tag_videos()
