c9e0a46536cf520b3b53ab23022a5edc689e3f11
Varun Ramesh <varunramesh@live.com>
2017-08-29 15:42:52 -0700

    Use shelf instead of requests-cache

--- dedupe.py
+++ dedupe.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import click
 import pinboard
-from collections import defaultdict
+import shelve
 
-import requests_cache
-requests_cache.install_cache()
+from collections import defaultdict
 
 from mercury_parser.client import MercuryParser
 
@@ -14,17 +13,26 @@ from mercury_parser.client import MercuryParser
 def dedupe(auth_token, mercury_api_key):
     pb = pinboard.Pinboard(auth_token)
     mercury = MercuryParser(api_key=mercury_api_key)
+    parse_cache = shelve.open('.parse_cache')
 
     by_url = defaultdict(list)
 
     with click.progressbar(pb.posts.all(), label="Finding duplicates...") as bar:
         for bookmark in bar:
-            parsed = mercury.parse_article(bookmark.url)
+            if bookmark.url in parse_cache:
+                parsed = parse_cache[bookmark.url]
+            else:
+                parsed = mercury.parse_article(bookmark.url)
+                parse_cache[bookmark.url] = parsed
+                parse_cache.sync()
+
             if not parsed: continue
             parsed = parsed.json()
             if 'url' not in parsed: continue
 
             by_url[parsed['url']].append(bookmark)
+
+    parse_cache.close()
 
     count = 0
     for url, items in by_url.items(): count += max(len(items) - 1, 0)
