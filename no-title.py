646a7773a5c906822d92ae934f6f4f6cea57a2b9
Varun Ramesh <varunramesh@live.com>
2018-07-28 00:28:11 -0700

    Improved script for finding articles with no title.

--- no-title.py
+++ no-title.py
@@ -5,57 +5,87 @@ import requests
 import time
 import sys
 from mercury_parser.client import MercuryParser
+from urlextract import URLExtract
+import urllib
+
+extractor = URLExtract()
+
+
+def is_url(text):
+    urls = extractor.find_urls(text)
+    return len(urls) == 1 and len(urls[0]) == len(text)
+
 
 @click.group()
-def cli(): pass
+def cli():
+    pass
+
+
+def has_no_title(link):
+    if link["description"] == False:
+        return True
+    if is_url(link["description"]):
+        return True
+    return False
+
 
 @cli.command()
-@click.option('--auth-token', required=True)
+@click.option("--auth-token", required=True)
 def list(auth_token):
-    links = requests.get('https://api.pinboard.in/v1/posts/all', params={
-        'auth_token': auth_token,
-        'format': 'json',
-    }).json()
+    username = auth_token.split(":")[0]
 
-    links = [l for l in links if l['href'] == l['description']]
+    links = requests.get(
+        "https://api.pinboard.in/v1/posts/all",
+        params={"auth_token": auth_token, "format": "json"},
+    ).json()
+
+    links = [l for l in links if has_no_title(l)]
 
     print(len(links), "articles without a title...")
     print("------------------")
     for link in links:
-        print("No title:", link['description'])
+        print(
+            f"https://pinboard.in/search/u:{username}?query={urllib.parse.quote(link['href'])}"
+        )
 
-@cli.command('add-titles')
-@click.option('--auth-token', required=True)
-@click.option('--mercury-api-key', required=True)
+
+@cli.command("add-titles")
+@click.option("--auth-token", required=True)
+@click.option("--mercury-api-key", required=True)
 def add_titles(auth_token, mercury_api_key):
     parser = MercuryParser(api_key=mercury_api_key)
 
-    links = requests.get('https://api.pinboard.in/v1/posts/all', params={
-        'auth_token': auth_token,
-        'format': 'json',
-    }).json()
+    links = requests.get(
+        "https://api.pinboard.in/v1/posts/all",
+        params={"auth_token": auth_token, "format": "json"},
+    ).json()
 
     for link in links:
-        href, title = link['href'], link['description']
-        if href == title:
-            print("No title:", href)
-            res = parser.parse_article(href)
+        if has_no_title(link):
+            res = parser.parse_article(link["href"])
             if res:
                 parsed = res.json()
-                print(parsed['title'])
-                time.sleep(1)
+                if parsed["title"] is None or parsed["title"].strip() == "":
+                    print(f"Can't find title for {link['href']}... Skipping...")
+                    continue
 
-                res = requests.get('https://api.pinboard.in/v1/posts/add', params={
-                    'auth_token': auth_token,
-                    'url': href,
-                    'description': parsed['title'],
-                    'format': 'json',
+                if click.confirm(
+                    f"Do you want to change the title of {link['href']} to \"{parsed['title']}\"?"
+                ):
+                    res = requests.get(
+                        "https://api.pinboard.in/v1/posts/add",
+                        params={
+                            "auth_token": auth_token,
+                            "url": link["href"],
+                            "description": parsed["title"],
+                            "format": "json",
+                            "extended": link["extended"],
+                            "toread": link["toread"],
+                            "tags": link["tags"],
+                            "shared": link["shared"],
+                        },
+                    )
 
-                    'extended': link['extended'],
-                    'toread': link['toread'],
-                    'tags': link['tags'],
-                    'shared': link['shared'],
-                })
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
